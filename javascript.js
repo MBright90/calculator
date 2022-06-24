9bab9214c9d962533866636ca43ee99f3fc7adf9
mbright90 <michael.bright90@icloud.com>
2022-06-24 11:08:43 +0100

    Adding addition feature
    Creating a function to allow the user to add two numbers together.
    This uses the dataset variables (previous, operation) to store the
    users selections before calling the calculateAnswer function.
    
    If the answer calculated is too large to display, an error message will
    display.

--- javascript.js
+++ javascript.js
@@ -1,21 +1,30 @@
+const screen = document.querySelector('#screen');
+
 // Initialize button variables //
 const numButtons = document.querySelectorAll('.numerical-btn');
 
 function initNumericButtons() {
     numButtons.forEach(numButton => {
         numButton.addEventListener('click', () => {
-            displayNewChar(numButton.dataset.numvalue);
+            if (screen.dataset.toClear == 'true') {
+                screen.textContent = 0;
+                screen.dataset.toClear = 'false';
+            };
+            displayNewChar(numButton.dataset.numValue);
         });
     });
 };
 
 const clearBtn = document.querySelector('#clear-btn');
-clearBtn.addEventListener('click', clearScreen)
+clearBtn.addEventListener('click', clearCalculator)
     
 
 const plusBtn = document.querySelector('#plus-btn');
 plusBtn.addEventListener('click', () => {
-    console.log('plus');
+    if (screen.dataset.operation) calculateAnswer();
+    screen.dataset.operation = 'plus';
+    storeNumber();
+    screen.dataset.toClear = 'true';
 });
 
 const minusBtn = document.querySelector('#minus-btn');
@@ -33,17 +42,24 @@ divideBtn.addEventListener('click', () => {
     console.log('divide');
 });
 
+const equalsBtn = document.querySelector('#equals-btn');
+equalsBtn.addEventListener('click', calculateAnswer);
+
+function storeNumber() {
+    screen.dataset.previous = screen.textContent;
+};
+
 
 // Screen functions //
 
 function displayNewChar(newCharacter) {
-    const screen = document.querySelector('#screen');
     let currentText = screen.textContent;
 
     if (currentText == '0') currentText = ''; 
 
     // Check characters do not pass length of screen //
     if (currentText.length <= 10) {
+        console.log(currentText.length);
         screen.textContent = `${currentText}${newCharacter}`;
     } else {
         alert('Maximum character length reached')
@@ -51,9 +67,38 @@ function displayNewChar(newCharacter) {
 };
 
 function clearCalculator() {
-    const screen = document.querySelector('#screen');
-    screen.textContent = '0';
+    screen.textContent = 0;
     screen.dataset.previous = '';
+    screen.dataset.operation = '';
+};
+
+
+// Mathematical functions //
+
+function addNumbers() {
+    console.log(`${screen.dataset.previous} ${screen.textContent}`)
+    return parseInt(screen.dataset.previous) + parseInt(screen.textContent);
+};
+
+// function subtractNumbers() {};
+
+// function multiplyNumbers() {};
+
+// function divideNumbers() {};
+
+function calculateAnswer() {
+    let answer = 0;
+    if (screen.dataset.operation == 'plus') {
+        answer = addNumbers();
+    };
+
+    if (toString(answer).length > 10) {
+        screen.textContent = 'TO LARGE!'
+    } else {
+        screen.textContent = answer;
+        screen.dataset.previous = answer;
+        screen.dataset.toClear = 'true'
+    };
 };
 
 initNumericButtons();
